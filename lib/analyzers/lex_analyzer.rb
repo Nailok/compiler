4489b64b6e9bf4845f7203202368d0cc66d149ab
Nailok <lexitor34@gmail.com>
2020-04-27 14:51:30 +0300

    Add hash with basic operators

---
+++ lib/analyzers/lex_analyzer.rb
@@ -0,0 +1,10 @@
+class LexAnalyzer
+  
+  def self.analyze(data)
+    tokens = { plus: '+', minus: '-', divider: '/', multiplier: '*', assignment: '=', comparsion: '==',
+               lesser: '<', greater: '>', lesser_or_equal: '<=', greater_or_equal: '>=', case: 'case',
+               when: 'when', from_to: '..', from_to_exclude_last: '...' }
+    
+
+  end
+end
